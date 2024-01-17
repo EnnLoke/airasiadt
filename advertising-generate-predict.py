a1f0d657fe1f0fcf0b5c5ea39e392fa9cc69a512
EnnLoke <156885166+EnnLoke@users.noreply.github.com>
2024-01-17 17:10:25 +0800

    Update advertising-generate-predict.py

--- advertising-generate-predict.py
+++ advertising-generate-predict.py
@@ -11,7 +11,7 @@ st.sidebar.header('User Input Parameters')
 def user_input_features():
     TV = st.sidebar.slider('TV', 0.7, 296.4, 148.5) 
     Radio = st.sidebar.slider('Radio', 0.0, 49.6,24.8 )
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 57.2)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 57.2)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
